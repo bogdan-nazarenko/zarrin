8d054b51528ca3f13e33bf6b38a32ebc3b62693d
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-02-15 00:10:55 +0200

    refactor: remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import reactLogo from "./assets/media/images/react.svg";
-
 const App = () => {
-    return (
-        <main>
-            <section>
-                <div className="container">
-                    <div className="hero">
-                        <img src={reactLogo} alt="React" />
-                        <h1>React + Vite + SCSS</h1>
-                    </div>
-                </div>
-            </section>
-        </main>
-    );
+    return <main></main>;
 };
 
 export default App;
