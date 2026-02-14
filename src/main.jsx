a24674af1f736023175a207d2e9ede44d0f1152c
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-02-15 00:58:29 +0200

    feat: add fonts

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/helpers/reset.scss";
 import "./styles/helpers/variables.scss";
+import "./styles/fonts.scss";
 import "./styles/main.scss";
 import App from "./App.jsx";
 
