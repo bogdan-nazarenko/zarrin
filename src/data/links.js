e3591d603dc8a5b781a8ae70db5a311503cd9568
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-09-15 20:54:03 +0300

    feat(data/links): add navLinks

---
+++ src/data/links.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+    { url: "/", name: "Home" },
+    { url: "/blog", name: "Blog" },
+    { url: "/about", name: "About" },
+    { url: "/contact", name: "Contact us" },
+];
