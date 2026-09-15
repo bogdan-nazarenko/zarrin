cf145caa5fdc647771ee5f21fce1524a672e1fa6
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-09-15 20:50:40 +0300

    feat(main): use router with basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import "@styles/index.scss";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <BrowserRouter basename="/zarrin/">
+            <App />
+        </BrowserRouter>
     </StrictMode>
 );
