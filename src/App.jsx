bcf0f59d1be755acce013ef004d95f403837d218
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-09-23 02:10:05 +0300

    feat(app): add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router";
 import Header from "@ui/layout/Header";
+const Home = lazy(() => import("@ui/pages/Home"));
 import Footer from "@ui/layout/Footer";
 
 const App = () => {
     return (
         <>
             <Header />
-            <main className="main"></main>
+            <main className="main">
+                <Suspense fallback={""}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </Suspense>
+            </main>
             <Footer />
         </>
     );
